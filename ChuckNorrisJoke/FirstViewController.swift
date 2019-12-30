d4f18102adf56c515bcd095ee5b634147accffbc
Ovchinnikov19 <r1dd1k1919@gmail.com>
2019-12-30 10:57:16 +0300

    Initial Commit

---
+++ ChuckNorrisJoke/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ChuckNorrisJoke
+//
+//  Created by Eduard Ovchinnikov on 30.12.2019.
+//  Copyright © 2019 Eduard Ovchinnikov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ChuckNorrisJoke/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ChuckNorrisJoke
+//
+//  Created by Eduard Ovchinnikov on 30.12.2019.
+//  Copyright © 2019 Eduard Ovchinnikov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
